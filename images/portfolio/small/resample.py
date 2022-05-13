3cf87796b52a7f6d25fcd254aea93b54472c90a7
Katia <k.demagkou@gmail.com>
2022-05-13 16:39:45 +0300

    'first_commit'

---
+++ images/portfolio/small/resample.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+from PIL import Image
+
+size = 600,400
+#size = 1200,800
+
+input_dir = 'C://Users//Bill//Desktop//Ceevee_2_0_0//images//portfolio//gallery'
+output_dir = os.getcwd()
+
+for i in range(len(os.listdir(input_dir))):
+	print('Reading -> ' + str(os.listdir(input_dir)[i]))
+	images = os.listdir(input_dir)
+	im = Image.open(input_dir + '//' + images[i])
+	im.thumbnail(size, Image.ANTIALIAS)
+	im.save(output_dir + '//' + images[i], "JPEG")
+	#im.save(output_dir + '//' + str(i) + '@2x.jpg', "JPEG")
